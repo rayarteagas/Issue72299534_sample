9c07407be7f0e0379a251614dfc637359656f50a
Gonzalo Fernandez and Raymond Arteaga <pair+gonzalof+ray@humana.com>
2020-03-03 10:42:14 -0300

    First commit

---
+++ app/src/main/java/com/muko/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.muko.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Activity2.class));
+            }
+        });
+
+    }
+}
